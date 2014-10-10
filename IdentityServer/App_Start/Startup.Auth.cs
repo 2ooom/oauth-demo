b2bd5f964eec924758ed16cb57e895a228887899
Dmitry <dmitry.parfenchik@gmail.com>
2014-10-10 15:51:00 +0200

    $1

---
+++ IdentityServer/App_Start/Startup.Auth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Infrastructure;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.Twitter;
+using Owin;
+
+namespace IdentityServer
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the Application Sign In Cookie.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Application",
+            });
+        }
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,15 @@
+﻿using IdentityServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly:OwinStartup(typeof(Startup))]
+namespace IdentityServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            ConfigureAuth(builder);
+        }
+    }
+}
