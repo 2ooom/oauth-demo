841854ab954ebb856cb75bab976ddb9edac506f5
Dmitry <dmitry.parfenchik@gmail.com>
2014-10-10 17:10:02 +0200

    $1

--- IdentityServer/App_Start/Startup.Auth.cs
+++ IdentityServer/App_Start/Startup.Auth.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.Owin;
-using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Infrastructure;
-using Microsoft.Owin.Security.OAuth;
-using Microsoft.Owin.Security.Twitter;
+﻿using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 namespace IdentityServer
--- IdentityServer/Controllers/AccountController.cs
+++ IdentityServer/Controllers/AccountController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using IdentityServer.Models;
+using Microsoft.Owin.Security;
 
 namespace IdentityServer.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Index()
+        public ActionResult Login()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var claims = new List<Claim> {new Claim(ClaimTypes.Name, loginViewModel.UserName)};
+            var identity = new ClaimsIdentity(claims, "Application");
+            Request.GetOwinContext().Authentication.SignIn(new AuthenticationProperties {IsPersistent = true}, identity);
             return View();
         }
     }
