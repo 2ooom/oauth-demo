766ddc4f274ee4a1647fb4c680ad0be48c04f101
Dmitry <dmitry.parfenchik@gmail.com>
2014-10-15 16:39:12 +0200

    $1

---
+++ IdentityServer/Data/AuthorizationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityServer.Data
+{
+    public class AuthorizationContext : IdentityDbContext<UserIdentity>
+    {
+        public AuthorizationContext()
+            :base("AuthorizationContext")
+        {
+            
+        }
+    }
+}
---
+++ IdentityServer/Data/AuthorizationRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityServer.Data
+{
+    public class AuthorizationRepository
+    {
+        private UserManager<UserIdentity> manager; 
+        private AuthorizationContext context; 
+        public AuthorizationRepository()
+        {
+            context = new AuthorizationContext();
+            manager = new UserManager<UserIdentity>(new UserStore<UserIdentity>(context));
+        }
+    }
+}
