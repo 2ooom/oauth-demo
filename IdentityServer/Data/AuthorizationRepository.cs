a2e4d33bd5f93113a25dc3d0057ef4f403880e43
2ooom <dmitry.parfenchik@gmail.com>
2014-10-16 15:35:24 +0200

    AuthorizationRepository added

--- IdentityServer/Data/AuthorizationRepository.cs
+++ IdentityServer/Data/AuthorizationRepository.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using IdentityServer.Models;
 
 namespace IdentityServer.Data
 {
     public class AuthorizationRepository
     {
         private UserManager<UserIdentity> manager; 
-        private AuthorizationContext context; 
+        private AuthorizationContext context;
+
         public AuthorizationRepository()
         {
             context = new AuthorizationContext();
             manager = new UserManager<UserIdentity>(new UserStore<UserIdentity>(context));
+        }
+
+        public async Task<IdentityResult> Create(LoginViewModel userModel)
+        {
+            var user = new UserIdentity { UserName = userModel.UserName };
+            return await manager.CreateAsync(user, userModel.Password);
+        }
+
+        public async Task<UserIdentity> Get(string username, string password)
+        {
+            return await manager.FindAsync(username, password);
         }
     }
 }
