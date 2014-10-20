a586a2a219bf9a148d093cd86b9e1ead2bb926ee
Dmitry <dmitry.parfenchik@gmail.com>
2014-10-20 14:27:05 +0200

    Authorized resource added

---
+++ src/OAuthDemo.Business.Host/Controllers/ProtectedController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OAuthDemo.Business.Host.Controllers
+{
+    [Authorize]
+    public class ProtectedController : Controller
+    {
+        // GET: /<controller>/
+        public IEnumerable<object> Get()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            return identity.Claims.Select(t => new
+            {
+                Type = t.Type,
+                Value = t.Value,
+            });
+        }
+    }
+}
